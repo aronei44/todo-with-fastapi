2af212731820443e147ee12e9089dfce3e257aeb
aronei44 <meliodasmeliodas224@gmail.com>
2022-08-28 08:56:11 +0700

    feat : cors

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import imp
 from fastapi import FastAPI 
 from migrations.main import Base, engine
 from routes import todoRoute
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def main():
