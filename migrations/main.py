0918ec20c2cc1da1b987f2b3dff221f709becfb7
aronei44 <meliodasmeliodas224@gmail.com>
2022-08-28 08:49:52 +0700

    feat : setting variable

--- migrations/main.py
+++ migrations/main.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import setting
 
-engine = create_engine("sqlite:///todo.db")
-engine = create_engine("postgresql://postgres:admin123@localhost:5432/todo")
+engine = create_engine("{}://{}:{}@{}:{}/{}".format(setting.DATABASE,setting.DATABASE_USERNAME,setting.DATABASE_PASSWORD,setting.DATABASE_URL,setting.DATABASE_PORT,setting.DATABASE_NAME))
 
 Base = declarative_base()
 
